53a1e01024295192d3f228997c17091595e113a1
Al04ni <albertjaynoxy@gmail.com>
2025-09-13 21:42:26 +0200

    cleaning for good

--- CodeWars/pete.py
+++
@@ -1,3 +0,0 @@
-available = {'flour': 76, 'cocoa': 81, 'sugar': 97} 
-ingredients ={'oil': 4407, 'chocolate': 9588, 'butter': 3578, 'crumbles': 6391, 'milk': 1172, 'eggs': 8567, 'sugar': 6247, 'cream': 8209, 'apples': 4520, 'flour': 7934}
-
